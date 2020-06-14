0be00bb6f0b311cb32ed5451ebcd13dfce93c887
Ingrid Banguero <ingrid_banguero@hotmail.com>
2020-06-14 12:23:04 -0500

    Change server configuration data

--- database/config.js
+++ database/config.js
@@ -2,7 +2,7 @@ const dbHost = "localhost";
 const dbName = "delilah_resto";
 const dbPort = "3306";
 const dbUser = "root";
-const dbpassword = "fernanda25"
+const dbpassword = ""
 const dbPath = `mysql://${dbUser}:${dbpassword}@${dbHost}:${dbPort}`;
 
 
